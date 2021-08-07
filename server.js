096718b46992e08e57c266e29efc038210eda9c0
Andradev101 <>
2021-08-07 18:33:10 +0000

    Made all locally

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
+function isURL(str) {
+  var urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
+  var url = new RegExp(urlRegex, 'i');
+  return str.length < 2083 && url.test(str);
+}
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,6 +26,32 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));//using body parser
+
+app.post('/api/shorturl', function (req, res){
+  originalurl = req.body.url;
+  shorturl  = 1;
+  if(isURL(originalurl)){
+    res.json({
+      original_url: `${originalurl}`,
+      short_url: `${shorturl}`
+    });
+  }else{
+    res.json({ error: 'invalid url' });
+  }
+})//getting the form from the form in originalurl
+
+app.get('/api/shorturl/:shorturl', function (req, res){
+  res.writeHead(301,
+    {Location: `${originalurl}`}
+  );
+  res.end();
+  //https://stackoverflow.com/questions/11355366/how-to-redirect-users-browser-url-to-a-different-page-in-nodejs
+})
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
